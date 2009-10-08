654185053fac7425bc56c83597d1657c25972f18
Paul Mucur <mudge@mudge.name>
2009-10-08 22:38:03 +0100

    Initial commit of a rethinking of my jQuery Example plugin.

---
+++ README.markdown
@@ -0,0 +1,14 @@
+jQuery Placeholder Plugin
+=========================
+
+A plugin to make HTML5's placeholder attribute work in non-HTML5-supporting
+browsers.
+
+This is currently a work in progress to simplify http://github.com/mudge/jquery_example
+and to delegate functionality to the browser where possible.
+
+Usage
+-----
+
+Simply use the `placeholder` attribute on your `input` tags and include 
+`jquery.placeholder.js` somewhere in your page.
---
+++ jquery.placeholder.js
@@ -0,0 +1,53 @@
+/*
+ * jQuery Placeholder Plugin.
+ * A plugin to make HTML5's placeholder attribute work in non-HTML5-supporting
+ * browsers.
+ *
+ * Copyright (c) Paul Mucur (http://mucur.name), 2009.
+ */
+(function($) {
+  
+  $.placeholder = {
+    
+    /* Allow the user to customise the class name of an example. */
+    className: '_placeholder',
+    
+    /* Determine whether the browser natively supports a placeholder.
+     * As seen in Mark Pilgrim's "Detecting HTML5 Features - Dive Into HTML5"
+     * http://diveintohtml5.org/detect.html#input-placeholder
+     */
+    supportedNatively: function() {
+      var testInput = document.createElement('input');
+      return 'placeholder' in testInput;
+    },
+    
+    backwardsCompatibility: function() {
+      if (!$.placeholder.supportedNatively()) {
+        $(window).unload(function() {
+          $('input.' + $.placeholder.className).val('');
+        });
+        
+        $('input[placeholder]').each(function() {
+          var $this = $(this);
+          var placeholder = $this.attr('placeholder');
+          
+          $this.blur(function() {
+            if ($this.val() == '') {
+              $this.addClass($.placeholder.className).val(placeholder);
+            }
+          }).focus(function() {
+            if ($this.hasClass($.placeholder.className)) {
+              $this.val('').removeClass($.placeholder.className);
+            }
+          }).parents('form:first').submit(function() {
+            $this.triggerHandler('focus');
+          }).end().triggerHandler('blur');
+        });
+      }
+    }
+  };
+  
+  $.placeholder.backwardsCompatibility();
+  
+})(jQuery);
+
